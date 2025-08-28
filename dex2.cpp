e86d40754d1be6e56b31dedcdc3b81e09fa1e655
Al-Kareem <albinaluber@gmail.com>
2025-08-28 21:40:30 +0700

    Perubahan ke 3

--- dex2.cpp
+++ dex2.cpp
@@ -1,10 +1,29 @@
 #include <iostream>
 using namespace std;
 
-int a1, a2, t, p, l;
+int a, b, t, p, l;
 
 int main() {
+// Perhitungan Keliling Persegi Panajng
   cout << "Keliling Persegi" << endl;
+  cout << "Panjang Persegi: ";
+  cin >> p;
+  cout << "Lebar Persegi: ";
+  cin >> l;
+  // rumus Luas= 2 x (p + l)
+  cout << "Hasil: " << (2 * (p + l)) << " cm" << endl;
+
+// Perhitungan Luas Trapesium
+cout << "Luas Trapesium" << endl;
+ cout << "Panjang Sisi a: ";
+  cin >> a;
+  cout << "Panjang Sisi b: ";
+  cin >> b;
+    cout << "Tinggi Trapesium: ";
+  cin >> t;
+  // rumus Luas= 2 x (p + l)
+  cout << "Hasil: " << 0.5 * (a + b) * t << " cm" <<endl;
+
 
 return 0;
 }
